03473d83fb3f50c9c0e2d62ad68e034f486c1d79
luniki <luniki@gmail.com>
2013-05-16 10:27:13 +0200

    reformat plugin

--- StudipMobile.php
+++ StudipMobile.php
@@ -7,13 +7,13 @@ class StudipMobile extends StudipPlugin implements SystemPlugin
      * This method dispatches and displays all actions. It uses the template
      * method design pattern, so you may want to implement the methods #route
      * and/or #display to adapt to your needs.
-                                                                             *
+     *
      * @param  string  the part of the dispatch path, that were not consumed yet
      */
     function perform($unconsumed_path) {
         $trails_root = $this->getPluginPath();
-        $dispatcher = new Trails_Dispatcher($trails_root, 
-                                            rtrim(PluginEngine::getURL($this, null, ''), '/'), 
+        $dispatcher = new Trails_Dispatcher($trails_root,
+                                            rtrim(PluginEngine::getURL($this, null, ''), '/'),
                                             self::DEFAULT_CONTROLLER);
         $dispatcher->plugin = $this;
         $dispatcher->dispatch($unconsumed_path);
