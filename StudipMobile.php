e00b42e75c2b22ac021171e5c72aadd56da2b754
luniki <luniki@gmail.com>
2013-05-16 10:26:22 +0200

    add Nobody role on plugin enable

--- StudipMobile.php
+++ StudipMobile.php
@@ -1,18 +1,4 @@
 <?php
-// Copyright (C) 2013  Nils Bussmann
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 class StudipMobile extends StudipPlugin implements SystemPlugin
 {
@@ -23,8 +9,6 @@ class StudipMobile extends StudipPlugin implements SystemPlugin
      * and/or #display to adapt to your needs.
                                                                              *
      * @param  string  the part of the dispatch path, that were not consumed yet
-     *
-     * @return void
      */
     function perform($unconsumed_path) {
         $trails_root = $this->getPluginPath();
@@ -34,6 +18,10 @@ class StudipMobile extends StudipPlugin implements SystemPlugin
         $dispatcher->plugin = $this;
         $dispatcher->dispatch($unconsumed_path);
     }
-}
 
-?>
+    // enable nobody role by default
+    static function onEnable($id)
+    {
+        RolePersistence::assignPluginRoles($id, array(7));
+    }
+}
