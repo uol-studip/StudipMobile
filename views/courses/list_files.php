2022dd6dc66d6f5317d76d21f4f03131c72307de
André Klaßen <andre.klassen@gmail.com>
2013-05-16 14:32:27 +0200

    refine title in course view

--- views/courses/list_files.php
+++ views/courses/list_files.php
@@ -1,26 +1,11 @@
 <?php
-// Copyright (C) 2013  Nils Bussmann
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-
-$this->set_layout("layouts/single_page_back");
-$page_title = "Kurs: ". htmlReady($course->name)." - Dateien";
+$page_title = "Kurs: ". htmlReady($course->name)." - Dateien"
 $page_id = "courses-list_files";
+$back_button = TRUE;
+$this->set_layout("layouts/single_page");
 
-if (sizeof($files)) 
-{
+if (sizeof($files)) {
     	?>
 	<a href="<?= $controller->url_for("courses/dropfiles", htmlReady($seminar_id)) ?>" class="externallink" data-ajax="false" data-role="button" data-theme="b">Alle Dateien in meine Dropbox</a><br>
 	<ul id="files" data-role="listview" data-filter="false" data-count-theme="b">
