c640e057d0d139f8dc84739dfc3b1dd045018113
luniki <luniki@gmail.com>
2013-05-21 09:44:37 +0200

    fix Couse#getMembers

--- models/course.php
+++ models/course.php
@@ -61,10 +61,9 @@ class Course {
                   FROM seminar_user
                   LEFT JOIN seminare  USING (Seminar_id)
                   LEFT JOIN schedule_seminare      ON (schedule_seminare.user_id='$user_id'
-                          AND schedule_seminare.seminar_id=seminare.Seminar_id)
+                        AND schedule_seminare.seminar_id=seminare.Seminar_id)
                   LEFT JOIN object_user_visits ouv ON (ouv.object_id=seminar_user.Seminar_id
-                          AND ouv.user_id='$user_id' AND ouv.type='sem')
-                  $add_query
+                        AND ouv.user_id='$user_id' AND ouv.type='sem')
                   WHERE seminar_user.user_id = '$user_id'";
 
         $stmt = \DBManager::get()->query($query);
@@ -79,12 +78,12 @@ class Course {
                   FROM   seminar_user
                   JOIN   auth_user_md5 ON auth_user_md5.user_id = seminar_user.user_id
                   JOIN   user_info     ON auth_user_md5.user_id = user_info.user_id
-                  WHERE  seminar_user.visible = 'yes' AND seminar_user.Seminar_id = '$semId'
+                  WHERE  seminar_user.visible = 'yes' AND seminar_user.Seminar_id = ?
                   ORDER BY FIELD(seminar_user.status, 'dozent','tutor' ,'autor', 'user'), auth_user_md5.Nachname
                   ";
-        $stmt = \DBManager::get()->query($query);
-        $result = $stmt->fetchAll();
-        return $result;
+        $stmt = \DBManager::get()->prepare($query);
+        $stmt->execute(array($semId));
+        return $stmt->fetchAll();
     }
 
     static function find($id)
