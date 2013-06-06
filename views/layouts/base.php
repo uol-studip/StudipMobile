2f37faa6f0874296398db02ee73e9b0df7b0a9f3
luniki <luniki@gmail.com>
2013-06-06 09:59:15 +0200

    use local scripts and stylesheets

--- views/layouts/base.php
+++ views/layouts/base.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, initial-scale=1, user-scalable=no">
 
     <title>Stud.IP Mobile</title>
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
+    <link rel="stylesheet" href="<?= $plugin_path ?>/public/vendor/jquery.mobile/jquery.mobile-1.3.1.min.css" />
     <link rel="stylesheet" href="<?= $plugin_path ?>/public/vendor/jquery.mobile/studip.min.css" />
     <link rel="stylesheet" href="<?= $plugin_path ?>/public/stylesheets/mobile.css" />
     <link rel="stylesheet"  href="<?= $plugin_path ?>/public/stylesheets/jquery.swipeButton.css" />
     <link rel="apple-touch-icon" href="<?= $plugin_path ?>/public/images/quickdial/ios.png" type="image/gif" />
 
-    <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-    <script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+    <script src="<?= $plugin_path ?>/public/vendor/jquery/jquery-1.9.1.min.js"></script>
+    <script src="<?= $plugin_path ?>/public/vendor/jquery.mobile/jquery.mobile-1.3.1.min.js"></script>
 
     <!-- MAP-->
     <script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
@@ -23,7 +23,7 @@
     <!-- CUSTOM END -->
     <script src="<?= $plugin_path ?>/public/vendor/date/date.js"></script>
     <script src="<?= $plugin_path ?>/public/javascripts/jquery.swipeButton.min.js"></script>
-    <script src="<?= $plugin_path ?>/public/javascripts/side_menu.min.js"></script>
+    <script src="<?= $plugin_path ?>/public/javascripts/side_menu.js"></script>
     <script>
       var STUDIP = STUDIP || {};
       STUDIP.ABSOLUTE_URI_STUDIP = "<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>";
